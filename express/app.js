61a8945793fb0ebb649e3ee138d815a15e9266da
computerphobe <aryanbharadiyak2006@gmail.com>
2026-02-07 18:13:01 +0530

    successfully connected DB to the server

--- express/app.js
+++ express/app.js
@@ -1,10 +1,47 @@
-const express = require('express')
+// imported modules
+
+import express from 'express';
+
+// Global variables
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) =>{
-    res.send("Hello World!")
+import { Client } from 'pg'
+const client = await new Client({
+    user:'postgres',
+    host: 'localhost',
+    database:'postgres',
+    password:'1234',
+    port:5432
 })
+await client.connect()// Hello world!
+
+app.get('/', (req, res) => {
+    const data = {
+        message: 'Hello world!',
+        status: "success",
+        timestamp: new Date().toISOString()
+    }
+    res.json(data)
+})
+
+app.get('/pg_data', async (req, res) => {
+    try {
+        // You MUST use 'await' here, or you get [object Promise]
+        const response = await client.query('SELECT $1::text as message', ['Hello world from DB']);
+        
+        console.log(`Data from pg: ${response.rows[0].message}`);
+
+        res.json({
+            message: response.rows[0].message,
+            status: 'success',
+        });
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).json({ error: "Failed to fetch data" });
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`Server started at ${port}`);
